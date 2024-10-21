4850933d08e66428227cac6313cc0676a214c966
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-10-21 14:56:04 +0200

    ✨ Added TableName.Get

---
+++ src/Drammer.Common.Infrastructure.Tests/Schema/TableNameTests.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Drammer.Common.Infrastructure.Schema;
+
+namespace Drammer.Common.Infrastructure.Tests.Schema;
+
+public sealed class TableNameTests
+{
+    [Fact]
+    public void Get_ReturnsTableName()
+    {
+        // arrange
+        var type = typeof(TestEntity);
+
+        // act
+        var result = TableName.Get(type);
+
+        // assert
+        result.Should().Be("test-table");
+    }
+
+    [Table("test-table")]
+    private sealed class TestEntity;
+}
---
+++ src/Drammer.Common.Infrastructure/Schema/TableName.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace Drammer.Common.Infrastructure.Schema;
+
+/// <summary>
+/// The table name functions.
+/// </summary>
+public static class TableName
+{
+    /// <summary>
+    /// Returns the name of the table.
+    /// </summary>
+    /// <param name="type">The entity type.</param>
+    /// <returns>A <see cref="string"/>.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the <see cref="TableAttribute"/> is not found.</exception>
+    public static string Get(Type type)
+    {
+        var attributes = type.GetCustomAttributes(typeof(TableAttribute)).ToList();
+
+        if (attributes == null || !attributes.Any())
+        {
+            throw new InvalidOperationException($"Type {type.Name} does not have a TableAttribute");
+        }
+
+        return ((attributes.First() as TableAttribute)!).Name;
+    }
+}

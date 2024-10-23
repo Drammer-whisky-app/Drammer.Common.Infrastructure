0a08b3922f9efaeb3a42e03df4ca770257901a03
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-10-23 16:10:45 +0200

    ✨ Added DatabaseFacadeExtensions

---
+++ src/Drammer.Common.Infrastructure.Tests/EntityFramework/DatabaseFacadeExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using Drammer.Common.Infrastructure.EntityFramework;
+
+namespace Drammer.Common.Infrastructure.Tests.EntityFramework;
+
+public sealed class DatabaseFacadeExtensionsTests
+{
+    [Fact]
+    public async Task ExecuteReaderAsync_ShouldNotThrowException()
+    {
+        // arrange
+        const string Sql = "SELECT * FROM Test";
+        var dbConnection = new MockDbConnection();
+
+        // act
+        var action = () => dbConnection.ExecuteReaderAsync(Sql, _ => new object());
+
+        // assert
+        await action.Should().NotThrowAsync<Exception>();
+    }
+
+    private class MockDbConnection : DbConnection
+    {
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ChangeDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        [AllowNull]
+        public override string ConnectionString { get; set; }
+
+        public override string Database { get; }
+        public override ConnectionState State => ConnectionState.Open;
+        public override string DataSource { get; }
+        public override string ServerVersion { get; }
+
+        protected override DbCommand CreateDbCommand()
+        {
+            return new MockDbCommand();
+        }
+    }
+
+    private class MockDbCommand : DbCommand
+    {
+        public override void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object? ExecuteScalar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        [AllowNull]
+        public override string CommandText { get; set; }
+
+        public override int CommandTimeout { get; set; }
+        public override CommandType CommandType { get; set; }
+        public override UpdateRowSource UpdatedRowSource { get; set; }
+        protected override DbConnection? DbConnection { get; set; }
+        protected override DbParameterCollection DbParameterCollection { get; }
+        protected override DbTransaction? DbTransaction { get; set; }
+        public override bool DesignTimeVisible { get; set; }
+
+        protected override DbParameter CreateDbParameter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+        {
+            return new Mock<DbDataReader>().Object;
+        }
+    }
+}
---
+++ src/Drammer.Common.Infrastructure/EntityFramework/DatabaseFacadeExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Drammer.Common.Infrastructure.EntityFramework;
+
+/// <summary>
+/// The database facade extensions.
+/// </summary>
+public static class DatabaseFacadeExtensions
+{
+    /// <summary>
+    /// Execute parameterized SQL using <see cref="Dapper"/>.
+    /// </summary>
+    /// <param name="database">The database.</param>
+    /// <param name="sql">The SQL.</param>
+    /// <param name="parseRowFunc">The parse row function.</param>
+    /// <param name="param">Query parameters.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <typeparam name="T">The result type.</typeparam>
+    /// <returns>An <see cref="IReadOnlyList{T}"/> containing the result.</returns>
+    [ExcludeFromCodeCoverage]
+    public static Task<IReadOnlyList<T>> ExecuteReaderAsync<T>(
+        this DatabaseFacade database,
+        string sql,
+        Func<DbDataReader, T?> parseRowFunc,
+        object? param = null,
+        CancellationToken cancellationToken = default)
+    {
+        // this connection is managed by EF and will not be closed in this function.
+        var connection = database.GetDbConnection();
+        return connection.ExecuteReaderAsync(sql, parseRowFunc, param, cancellationToken);
+    }
+
+    internal static async Task<IReadOnlyList<T>> ExecuteReaderAsync<T>(
+        this DbConnection connection,
+        string sql,
+        Func<DbDataReader, T?> parseRowFunc,
+        object? param = null,
+        CancellationToken cancellationToken = default)
+    {
+        var result = new List<T>();
+
+        DbDataReader? reader = null;
+        try
+        {
+            reader = await connection.ExecuteReaderAsync(
+                sql,
+                param);
+
+            if (reader.HasRows)
+            {
+                while(await reader.ReadAsync(cancellationToken))
+                {
+                    var data = parseRowFunc(reader);
+                    if (data != null)
+                    {
+                        result.Add(data);
+                    }
+                }
+            }
+        }
+        finally
+        {
+            if (reader != null)
+            {
+                await reader.CloseAsync();
+            }
+        }
+
+        return result.AsReadOnly();
+    }
+}
